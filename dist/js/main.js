1eaad1617800f489f400e82456c6951839b3894b
Catalin B <catalin.bac@gmail.com>
2023-06-15 14:17:08 +0300

    scroll to top added

--- dist/js/main.js
+++ dist/js/main.js
@@ -183,6 +183,28 @@ let layoutHandler;
 
 
 
+  // Start scroll top
+  let btn = $('#button');
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+  });
+
+  // End scroll top
+
+
+
+
+
 
 
 })();
